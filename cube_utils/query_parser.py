418948567ab8ead10d179b1a2b286a31ee67bc09
Julio Avila <julioley32@gmail.com>
2025-06-05 15:07:18 -0500

    add logic for pushed down queries in measures and dimensions (#4)
    * add logic for pushed down queries in measures and dimensions
    
    * add filters and members to push down logic check
    
    * Add validation to pushdown members
    
    * refactor member validation
    
    * refactor member validation
    
    * refactor member validation
    
    ---------
    
    Co-authored-by: Francisco Valdez de la Fuente <paco.valdez@berkeley.edu>

--- cube_utils/query_parser.py
+++ cube_utils/query_parser.py
@@ -1,8 +1,25 @@
 from typing import List, Dict, Any
 
 
+def is_pushdown_member(member: Any) -> bool:
+    """
+    Check if a member is a pushdown member.
+    A pushdown member is a dictionary with 'cubeName' and 'expressionName' keys.
+    :param member: The member to check.
+    :return: True if the member is a pushdown member, False otherwise.
+    """
+    return (
+        isinstance(member, dict) and "cubeName" in member and "expressionName" in member
+    )
+
+
 # Function to extract cubes from a query payload
 def extract_cubes(payload: Dict[str, Any]) -> List[str]:
+    """
+    Extracts unique cubes from the given query payload.
+    :param payload: The query payload containing dimensions, measures, filters, segments, and time dimensions.
+    :return: A list of unique cube names.
+    """
     cubes = set()
     members = extract_members(payload)
     for member in members:
@@ -13,39 +30,47 @@ def extract_cubes(payload: Dict[str, Any]) -> List[str]:
 
 # Function to extract cube members
 def extract_members(payload: Dict[str, Any]) -> List[str]:
+    """
+    Extracts unique members from the given query payload.
+    :param payload: The query payload containing dimensions, measures, filters, segments, and time dimensions.
+    :return: A list of unique members in the format 'cubeName.expressionName'.
+    """
     members = set()  # Use a set to ensure uniqueness
 
-    # Extract cubes from dimensions
-    if "dimensions" in payload:
-        for dimension in payload["dimensions"]:
-            members.add(dimension)
+    query_keys = [
+        "dimensions",
+        "measures",
+        "filters",
+        "segments",
+        "timeDimensions",
+    ]
 
-    # Extract cubes from measures
-    if "measures" in payload:
-        for measure in payload["measures"]:
-            members.add(measure)
-
-    # Extract cubes from filters
-    if "filters" in payload:
-        for filter_item in payload["filters"]:
-            members.update(extract_members_from_filter(filter_item))
-
-    # Extract cubes from segments
-    if "segments" in payload:
-        for segment in payload["segments"]:
-            members.add(segment)
-
-    # Extract cubes from timeDimensions
-    if "timeDimensions" in payload:
-        for time_dimension in payload["timeDimensions"]:
-            if "dimension" in time_dimension:
-                members.add(time_dimension["dimension"])
+    for key in query_keys:
+        if key in payload:
+            for item in payload[key]:
+                if is_pushdown_member(item):
+                    members.add(f"{item['cubeName']}.{item['expressionName']}")
+                elif key == "filters":
+                    members.update(extract_members_from_filter(item))
+                elif (
+                    key == "timeDimensions"
+                    and isinstance(item, dict)
+                    and "dimension" in item
+                ):
+                    members.add(item["dimension"])
+                else:
+                    members.add(item)
 
     return list(members)
 
 
 # Extracts filters and handles boolean logic recursively
-def extract_members_from_filter(filter_item):
+def extract_members_from_filter(filter_item: Dict[str, Any]) -> set:
+    """
+    Extracts members from a filter item, handling boolean logic (AND/OR) recursively.
+    :param filter_item: The filter item to extract members from.
+    :return: A set of unique members extracted from the filter item.
+    """
     members = set()
 
     # Handle direct member filters
